3eaa0fbee5b503539784e6124013403ae3b47cbd
ghiannir <ghiannir@gmail.com>
2022-11-18 11:12:23 +0100

    Modified header

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Nav, NavItem } from 'react-bootstrap';
+//import { Nav, NavItem } from 'react-bootstrap';
 import 'bulma/css/bulma.min.css';
-import { Button } from 'react-bulma-components';
+import { Navbar, Button } from 'react-bulma-components';
 
-import NavbarBrand from './NavbarBrand';
+//import NavbarBrand from './NavbarBrand';
 
 import { default as api } from '../api';
 import { parseDate, isNotificationsActive, usePolling } from '../utils';
@@ -53,43 +53,49 @@ const Header = () => {
 
   return (
     <React.Fragment>
-      <Nav className="mr-auto navbar-nav">
-        <NavItem className="ml-3 mr-auto">
+      <Navbar className="nav is-light" fixed="top">
+        <Navbar.Container align="left">
+        <Navbar.Item
+        className={`mx-auto ${isNotificationsActive() ? null : 'pr-5'} logo`}
+        renderAs='a'
+        href="#"
+        //full={brandFull}
+        //minimized={brandMinimized}
+      /><img src={logo} width={brandFull.width} height={brandFull.height}/><Navbar.Item/>
+        <Navbar.Item className="ml-3 mr-auto">
           <Link to="/" className="nav-link">
             Dashboard
           </Link>
-        </NavItem>
-        <NavItem className="mr-auto">
+        </Navbar.Item>
+        <Navbar.Item className="mr-auto">
           {isNotificationsActive() ? null : (
             <Button className="button is-light is-small" onClick={() => getMessageToken()}>
               Notifiche
             </Button>
           )}
-        </NavItem>
-      </Nav>
-      <NavbarBrand
-        className={`mx-auto ${isNotificationsActive() ? null : 'pr-5'} logo`}
-        full={brandFull}
-        minimized={brandMinimized}
-      />
-      <Nav className="ml-auto navbar-nav">
+        </Navbar.Item>
+      {/* </Navbar> */}</Navbar.Container>
+      
+      {/* <Navbar color="link" fixed="top"> */}
+      <Navbar.Container align="right">
         {show ? (
-          <NavItem className="mr-2 ml-auto">
+          <Navbar.Item className="mr-2 ml-auto">
             <div className="blink px-1">{bike} on the road</div>
-          </NavItem>
+          </Navbar.Item>
         ) : null}
 
-        <NavItem className="mr-3 ml-auto">
+        <Navbar.Item className="mr-3 ml-auto">
           <Link to="/credits" className="nav-link">
             Credits
           </Link>
-        </NavItem>
-        <NavItem className="ml-auto mr-3">
+        </Navbar.Item>
+        <Navbar.Item className="ml-auto mr-3">
           <Link to={api.isLogged() ? '/logout' : '/login'} className="nav-link">
             {api.isLogged() ? 'Logout' : 'Login'}
           </Link>
-        </NavItem>
-      </Nav>
+        </Navbar.Item>
+        </Navbar.Container>
+      </Navbar>
     </React.Fragment>
   );
 };
